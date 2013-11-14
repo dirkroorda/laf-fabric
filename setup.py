22c6cd1c8f533a38c7b27b313e475f681aff0f3f
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2013-11-14 12:50:07 +0100

    update docs

--- setup.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf8 -*-
-from distutils.core import setup
-setup(name='graf',
-      version='0.1',
-      author='Dirk Roorda',
-      author_email='dirk.roorda@dans.knaw.nl',
-      description='Workbench to read LAF resources (Linguistic Annotation Framework ISO 24612:2012) and analyse them efficiently',
-      packages=['graf']
-)

d9223ff43ebc3dfd87839c7f946522754d87f0a4
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2014-01-10 11:32:26 +0100

    New minor release 2.3.1

---
+++ tasks/annox_create_clause.py
@@ -0,0 +1,243 @@
+import sys
+import collections
+
+load = {
+    "primary": True,
+    "xmlids": {
+        "node": True,
+        "edge": False,
+    },
+    "features": {
+        "shebanq": {
+            "node": [
+                "db.otype,oid,monads",
+                "ft.text",
+                "sft.book,chapter,verse",
+                "ft.clause_type,clause_constituent_relation,clause_atom_number,clause_atom_relation,clause_atom_relation_kind,clause_atom_type",
+                "ft.clause_atom_relation_daughter_tense,clause_atom_relation_mother_tense,clause_atom_relation_preposition_class",
+            ],
+        },
+    },
+}
+
+config = {
+    'target_types': [
+        'clause',
+        'clause_atom',
+    ],
+    'show_features': {
+        'shebanq': {
+            'node': [
+                "ft.clause_atom_number,clause_type,clause_atom_type,clause_constituent_relation,clause_atom_relation,clause_atom_relation_kind",
+                "ft.clause_atom_relation_daughter_tense,clause_atom_relation_mother_tense,clause_atom_relation_preposition_class",
+            ],
+        },
+    },
+    'new_features': {
+        'gino': {
+            'node': [
+                "ft.clause_atom_relation",
+            ],
+        },
+    },
+    'passages': {
+        'Psalms': '1-150',
+    },
+}
+
+target_types = config['target_types']
+type_col = {}
+n_types = len(target_types)
+for (n, tt) in enumerate(target_types):
+    type_col[tt] = n
+
+new_features_spec = config['new_features']
+new_fqnames = []
+new_fnames = []
+new_features = []
+for aspace in sorted(new_features_spec.keys()):
+    for kind in sorted(new_features_spec[aspace].keys()):
+        for line in new_features_spec[aspace][kind]:
+            (alabel, fnames) = line.split('.')
+            for fname in fnames.split(','):
+                fqname = "{}:{}.{}".format(aspace, alabel, fname)
+                new_fqnames.append(fqname)
+                new_features.append((aspace, alabel, fname))
+                new_fnames.append(fname)
+n_new_features = len(new_fqnames)
+
+show_features_spec = config['show_features']
+show_fqnames = []
+show_fnames = []
+show_features = []
+for aspace in sorted(show_features_spec.keys()):
+    for kind in sorted(show_features_spec[aspace].keys()):
+        for line in show_features_spec[aspace][kind]:
+            (alabel, fnames) = line.split('.')
+            for fname in fnames.split(','):
+                fqname = "{}:{}.{}".format(aspace, alabel, fname)
+                show_fqnames.append(fqname)
+                show_features.append((aspace, alabel, fname))
+                show_fnames.append(fname.replace('_',' '))
+n_show_features = len(show_fqnames)
+
+def nonerep(val):
+    return '' if val == None else val
+
+def task(graftask):
+    '''Workflow to create new annotations, specialized for clauses.
+
+    There are two modes:
+
+    1. create a blank form (tab delimited file)
+    2. create annotations from a filled-in form
+
+    The dictionary ``config`` contains the specifications for creating the form.
+    The same specifications are used when reading the filled in form.
+
+    You specify node types that you want to annotate, e.g. *word* and *phrase*.
+    You specifiy new features for which you want to provide values, e.g. 
+    ``dirk:part.intro`` and ``dirk:part.role``.
+    Finally you specify some chapters in the bible that you want to annotate.
+
+    **Mode 1. creates a tab-delimited spreadsheet**
+
+    It will have the following columns:
+
+    A:
+        xml_id of the node
+
+    D1, ... , Dn:
+        for each node type Di a column, with in it the piece of primary text corresponding to it.
+        So if in our case a column for ``word`` and for ``phrase``.
+        If the node is a ``word``, the ``word`` column will be filled with the primary text of that word,
+        and the ``phrase`` column will be empty. If the node is a ``phrase``, it is the other way round, 
+        with the ``phrase`` content in the ``phrase`` column.
+
+    S1, ... , Sm:
+        for each feature Si a column where that feature is displayed.
+
+    N1, ... , Nk
+        for each new feature Ni a column.
+        These contain the cells where you can put in your feature values.
+        If you put the value 'A' in column ``dirk:part.intro`` on the row of node ``n``, 
+        you declare that node ``n`` has ``dirk:part.intro = 'A'``.
+
+    **Mode 2. creates a LAF annotation file**
+
+    If you have filled in the form and renamed it (replacing ``form`` in the file name by ``data``),
+    then mode 2 can do its work. It creates a file with a name that starts with ``annot``.
+    It will transform the information that you have put into the spreadsheet into properly targeted annotations.
+
+    **Using the annotations**
+
+    If you move the ``annot`` file over to the ``annox`` directory, and include it in a ``_header_.xml``
+    file, you can use the annotations in tasks by selecting it as an annox.
+    See the task :mod:`annox_use`.
+    '''
+    (msg, P, NN, F, X) = graftask.get_mappings()
+
+    def make_form():
+        msg("Reading the books ...")
+        outf = graftask.add_result("form_{}={}.txt".format('_'.join(target_types), '_'.join(new_fnames)))
+
+        the_book = None
+        the_chapter = None
+        the_verse = None
+        in_book = False
+        in_chapter = False
+        do_chapters = {}
+        outf.write("{}\t{}\t{}\t{}\n".format('passage', "\t".join(target_types), "\t".join(show_fnames), "\t".join(new_fqnames)))
+        for i in NN():
+            this_type = F.shebanq_db_otype.v(i)
+            if this_type in target_types:
+                if in_chapter:
+                    the_xml_id = X.node.r(i)
+                    the_text = " ".join([text for (n, text) in P.data(i)])
+                    the_show_features = "\t".join([nonerep(F.F["{}_{}_{}".format(*sf)][i]) for sf in show_features])
+                    outf.write("{}\t{}{}{}{}\t{}\n".
+                        format(the_xml_id, "\t" * type_col[this_type], the_text, "\t" * (n_types - type_col[this_type]), the_show_features, "\t" * n_new_features))
+            elif this_type == "book":
+                the_book = F.shebanq_sft_book.v(i)
+                in_book = the_book in config['passages']
+                if in_book:
+                    sys.stderr.write(the_book)
+                    do_chapters = {}
+                    chapter_ranges = config['passages'][the_book].split(',')
+                    for chapter_range in chapter_ranges:
+                        boundaries = chapter_range.split('-')
+                        (b, e) = (None, None)
+                        if len(boundaries) == 1:
+                            b = int(chapter_range)
+                            e = int(chapter_range) + 1
+                        else:
+                            b = int(boundaries[0])
+                            e = int(boundaries[1]) + 1
+                        for chapter in range(b, e):
+                            do_chapters[str(chapter)] = None
+                else:
+                    sys.stderr.write("*")
+            elif this_type == "chapter":
+                if in_book:
+                    the_chapter = F.shebanq_sft_chapter.v(i)
+                    if the_chapter in do_chapters:
+                        sys.stderr.write("{},".format(the_chapter))
+                        in_chapter = True
+                    else:
+                        in_chapter = False
+                else:
+                    in_chapter = False
+            elif this_type == "verse":
+                if in_chapter:
+                    the_verse = F.shebanq_sft_verse.v(i)
+                    outf.write("#{} {}:{}\t{}\t{}\n".format(the_book, the_chapter, the_verse, "\t" * n_types, "\t" * n_new_features))
+        sys.stderr.write("\n")
+
+    def make_annots():
+        inp = graftask.add_input("data_{}={}.txt".format('_'.join(target_types), '_'.join(new_fnames)))
+        outa = graftask.add_result("annot_{}={}.xml".format('_'.join(target_types), '_'.join(new_fnames)))
+        outa.write('''<?xml version="1.0" encoding="UTF-8"?>
+<graph xmlns="http://www.xces.org/ns/GrAF/1.0/" xmlns:graf="http://www.xces.org/ns/GrAF/1.0/">
+    <graphHeader>
+        <labelsDecl/>
+        <dependencies/>
+        <annotationSpaces/>
+    </graphHeader>
+''')
+        aid = 0
+        header = True
+        features = collections.defaultdict(lambda: collections.defaultdict(lambda: collections.defaultdict(lambda: {})))
+        for line in inp:
+            if header:
+                header = False
+                continue
+            if line.startswith('#'):
+                continue
+            fields = line.rstrip().split("\t")
+            xml_id = fields[0]
+            values = fields[n_types + n_show_features + 1:n_types + n_show_features + 1 + n_new_features]
+            for (n, value) in enumerate(values): 
+                if value == "":
+                    continue
+                (aspace, alabel, fname) = new_features[n]
+                features[aspace][alabel][xml_id][fname] = value
+
+        for aspace in features:
+            for alabel in features[aspace]:
+                for xml_id in features[aspace][alabel]:
+                    aid += 1
+                    outa.write('<a xml:id="a{}" as="{}" label="{}" ref="{}"><fs>\n'.format(aid, aspace, alabel, xml_id))
+                    for fname in features[aspace][alabel][xml_id]:
+                        value = features[aspace][alabel][xml_id][fname]
+                        outa.write('\t<f name="{}" value="{}"/>\n'.format(fname, value))
+                    outa.write('</fs></a>\n')
+        outa.write("</graph>\n")
+
+    answer = input("1 = make form, 2 = make annotations [1/2] ")
+    if answer == "1":
+        make_form()
+    elif answer == "2":
+        make_annots()
+
+
+
---
+++ tasks/cooccurrence.py
@@ -0,0 +1,187 @@
+import sys
+import collections
+from IPython import embed
+
+load = {
+    "xmlids": {
+        "node": False,
+        "edge": False,
+    },
+    "features": {
+        "shebanq": {
+            "node": [
+                "db.otype",
+                "ft.part_of_speech,noun_type,lexeme_utf8",
+                "sft.book",
+            ],
+            "edge": [
+            ],
+        },
+    },
+}
+
+def task(graftask):
+    '''Collect co-occurrence data for lexemes across books in the bible.
+
+    This is work together with Martijn Naaijer.
+
+    We construct tables to load into Gephi (graph analysis).
+    Nodes: the bible books.
+    Edges: for each distinct lexeme that occurs in book1 and book2, we construct an edge.
+    N.B.: we get multiple edges between pairs of nodes.
+    Edges will be weighted.
+   
+    * The first weigth factor is only dependent on the lexeme: it is 1/s(lexeme)
+      where s(lexeme) is the support for lexeme, defined as the number of books in which the lexeme occurs.
+    * The second weight factor is dependent on the books: it is 1/#(book1 + book2)
+      where book1 + book2 is the union of the sets of lexemes in book1 and book2, and #(set)
+      is the cardinality of the set.
+
+    Returns:
+        The data will be delivered in two tab separated files: nodes.tsv for nodes, and edges.tsv for edges.
+    
+    Node format
+        id <tab> label <newline>
+
+    Edge format
+        id <tab> source <tab> target <tab> type <tab> label <tab> weight 
+
+    The type is always 1, meaning: bidirectional. 
+
+
+    '''
+    (msg, P, NN, F, X) = graftask.get_mappings()
+
+    lexemes = collections.defaultdict(lambda: collections.defaultdict(lambda:collections.defaultdict(lambda:0)))
+    lexeme_support_book = collections.defaultdict(lambda: collections.defaultdict(lambda: {}))
+
+    tasks = {
+        'noun_common': {
+            '1': graftask.add_result("noun_common_1.gexf"),
+            '2': graftask.add_result("noun_common_2.gexf"),
+        },
+        'noun_proper': {
+            '1': graftask.add_result("noun_proper_1.gexf"),
+            '2': graftask.add_result("noun_proper_2.gexf"),
+        },
+        'verb': {
+            '1': graftask.add_result("verb_1.gexf"),
+            '2': graftask.add_result("verb_2.gexf"),
+        },
+        'all': {
+            '1': graftask.add_result("all_1.gexf"),
+            '2': graftask.add_result("all_2.gexf"),
+        },
+    }
+
+    methods = {
+        '1': lambda x, y: float(x) / y,
+        '2': lambda x, y: float(x) / y / y,
+    }
+
+    book_name = None
+    books = []
+
+    data_header = '''<?xml version="1.0" encoding="UTF-8"?>
+<gexf xmlns:viz="http:///www.gexf.net/1.2draft/viz" xmlns="http://www.gexf.net/1.1draft" version="1.2">
+<meta>
+<creator>LAF-Fabric</creator>
+</meta>
+<graph defaultedgetype="undirected" idtype="string" type="static">
+'''
+    do_multigraph = False
+    '''If there are multiple edges between a pair of nodes, the graph is called amultigraph.
+    Gephi versions <0.9 do not support multigraphs.
+    
+    In the multigraph case, we create an edge for each lexeme that is common to a pair of books.
+    In the other case, we add the weights of individual lexeme-labeled edges into the weight of 
+    a single, total edge.
+    '''
+     
+
+    for node in NN():
+        this_type = F.shebanq_db_otype.v(node)
+        if this_type == "word":
+            lexeme = F.shebanq_ft_lexeme_utf8.v(node)
+
+            lexemes['all'][book_name][lexeme] += 1
+            lexeme_support_book['all'][lexeme][book_name] = 1
+
+            p_o_s = F.shebanq_ft_part_of_speech.v(node)
+            if p_o_s == "noun":
+                noun_type = F.shebanq_ft_noun_type.v(node)
+                if noun_type == "common":
+                    lexemes['noun_common'][book_name][lexeme] += 1
+                    lexeme_support_book['noun_common'][lexeme][book_name] = 1
+                elif noun_type == "proper":
+                    lexemes['noun_proper'][book_name][lexeme] += 1
+                    lexeme_support_book['noun_proper'][lexeme][book_name] = 1
+            elif p_o_s == "verb":
+                lexemes['verb'][book_name][lexeme] += 1
+                lexeme_support_book['verb'][lexeme][book_name] = 1
+
+        elif this_type == "book":
+            book_name = F.shebanq_sft_book.v(node)
+            books.append(book_name)
+            sys.stderr.write("{} ".format(book_name))
+    sys.stderr.write("\n")
+
+    nodes_header = '''<nodes count="{}">\n'''.format(len(books))
+
+    for this_type in tasks:
+
+        lexeme_support = {}
+        for lexeme in lexeme_support_book[this_type]:
+            lexeme_support[lexeme] = len(lexeme_support_book[this_type][lexeme])
+             
+        book_size = collections.defaultdict(lambda: 0)
+        for book in lexemes[this_type]:
+            book_size[book] = len(lexemes[this_type][book])
+             
+        node_data = []
+        for node in range(len(books)):
+            node_data.append('''<node id="{}" label="{}"/>\n'''.format(node + 1, books[node]))
+
+        edge_id = 0
+        edge_data = collections.defaultdict(lambda: [])
+        for src in range(len(books)):
+            for tgt in range(src + 1, len(books)):
+                book_src = books[src]
+                book_tgt = books[tgt]
+                lexemes_src = {}
+                lexemes_tgt = {}
+                lexemes_src = lexemes[this_type][book_src]
+                lexemes_tgt = lexemes[this_type][book_tgt]
+                intersection_size = 0
+                weights = collections.defaultdict(lambda: 0)
+                for lexeme in lexemes_src:
+                    if lexeme not in lexemes_tgt:
+                        continue
+                    pre_weight = lexeme_support[lexeme]
+                    for this_method in tasks[this_type]:
+                        weights[this_method] += methods[this_method](1000, pre_weight)
+                    intersection_size += 1
+                combined_size = book_size[book_src] + book_size[book_tgt] - intersection_size
+                edge_id += 1
+                for this_method in tasks[this_type]:
+                    edge_data[this_method].append('''<edge id="{}" source="{}" target="{}" weight="{:.3g}"/>\n'''.
+                        format(edge_id, src + 1, tgt + 1, weights[this_method]/combined_size))
+                
+        for this_method in tasks[this_type]:
+            edges_header = '''<edges count="{}">\n'''.format(len(edge_data[this_method]))
+            out_file = tasks[this_type][this_method]
+            out_file.write(data_header)
+
+            out_file.write(nodes_header)
+            for node_line in node_data:
+                out_file.write(node_line)
+            out_file.write("</nodes>\n")
+
+            out_file.write(edges_header)
+            for edge_line in edge_data[this_method]:
+                out_file.write(edge_line)
+            out_file.write("</edges>\n")
+            out_file.write("</graph></gexf>\n")
+
+        sys.stdout.write("{}: nodes:  {}\nedges: {}\n".format(this_type, len(books), edge_id))
+

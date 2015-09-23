6788dfd2ba5c2e1470acbb244d448e63d3de8859
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2015-09-23 17:47:31 +0200

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -38,9 +38,6 @@ if not on_rtd:
     import sphinx_rtd_theme
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-else:
-    html_theme_path = ['.']
-    html_theme = 'default'
 
 html_static_path = ['_static']
 html_domain_indices = True

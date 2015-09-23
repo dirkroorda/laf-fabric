e225bf0026f34740b3dbb58ab8cfb7ca9d5d3b0c
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2015-09-23 17:45:38 +0200

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -39,7 +39,7 @@ if not on_rtd:
     html_theme = 'sphinx_rtd_theme'
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 else:
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    html_theme_path = ['.']
     html_theme = 'default'
 
 html_static_path = ['_static']
